70f38ecb00bb38aaa3bf9cc57056b071086de0af
賴彥廷 <alex85amy@gmail.com>
2023-11-23 15:14:56 +0800

    update

--- src/main/test/BCryptExample.java
+++ src/main/test/BCryptExample.java
@@ -6,6 +6,11 @@ public class BCryptExample {
 		// 加密密码
         String rawPassword = "123";
         String hashedPassword = BCrypt.hashpw(rawPassword, BCrypt.gensalt());
-        System.out.println("Hashed Password: " + hashedPassword);
+        System.out.println("Hashed Password: " + hashedPassword);//$2a$10$tDzvATEP0SPFucbrCcYKUOGZ6s/KtvfIpVM3TRd8LlzrqCI/F0hiu
+	
+        //驗證密碼
+        String userPasswod = "123";//模擬用戶輸入的密碼(明碼)
+        boolean isPasswordMatch = BCrypt.checkpw(userPasswod,"$2a$10$tDzvATEP0SPFucbrCcYKUOGZ6s/KtvfIpVM3TRd8LlzrqCI/F0hiu");
+        System.out.println("Password: "+isPasswordMatch);
 	}
 }
